9027e982905522901430fe338f814e844a09663a
davejuanes <dmjuanes_@hotmail.es>
2022-08-15 16:26:58 -0400

    FUNCION PARA HALLAR LA MODA

--- platzimath.js
+++ platzimath.js
@@ -6,7 +6,30 @@ function esImpar(lista) {
     return lista.length % 2
 }
 
-function calcularMediana(lista) {
+function calcularModa(lista) {
+    const listaCount = {}
+
+    for (let i = 0; i < lista.length; i++) {
+        const elemento = lista[i]
+
+        if (listaCount[elemento]) {
+            listaCount[elemento] += 1
+        } else {
+            listaCount[elemento] = 1
+        }        
+    }
+    const listaArray = Object.entries(listaCount)
+    const listaOrdenada = ordernarListaBidimensional(listaArray, 1)
+    const listaMaxNumber = listaOrdenada[listaOrdenada.length - 1]
+    // console.log(listaCount, listaArray, listaOrdenada, listaMaxNumber);
+    // console.log('La moda es: '+ listaMaxNumber[0]);
+
+    const moda = listaMaxNumber[0]
+    return moda
+}
+
+function calcularMediana(listaDesordenada) {
+    const lista = ordernarLista(listaDesordenada)
     const listaEsPar = esPar(lista)
 
     if (listaEsPar) {
@@ -57,3 +80,31 @@ function calcularPromedio(lista) {
     return promedio
 }
 
+function ordernarLista(listaDesordenada) {
+    function ordenarListaSort(valorAcumulado, nuevoValor) {
+        /* if (valorAcumulado < nuevoValor) {
+            return -1
+        } else if (valorAcumulado == nuevoValor) {
+            return 0
+        } else if (valorAcumulado > nuevoValor) {
+            return 1
+        } */
+
+        //return nuevoValor - valorAcumulado //Ordena ASC
+        return valorAcumulado - nuevoValor //Ordena DESC
+    }
+
+    const lista = listaDesordenada.sort(ordenarListaSort)
+
+    return lista
+}
+// [ [], [], [], []]
+function ordernarListaBidimensional(listaDesordenada, i) {
+    function ordenarListaSort(valorAcumulado, nuevoValor) {
+        return valorAcumulado[1] - nuevoValor[1]
+    }
+
+    const lista = listaDesordenada.sort(ordenarListaSort)
+
+    return lista
+}
